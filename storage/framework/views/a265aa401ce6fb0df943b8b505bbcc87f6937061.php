4a49c42f149bd5524060ffeff1f2028ef64216b6
JorgeMTrinidad <61073738+JorgeMTrinidad@users.noreply.github.com>
2021-10-18 20:13:46 -0600

    Ultimo previamente correcto

--- storage/framework/views/a265aa401ce6fb0df943b8b505bbcc87f6937061.php
+++ storage/framework/views/a265aa401ce6fb0df943b8b505bbcc87f6937061.php
@@ -22,14 +22,6 @@
 
 
     <div class="form-group row">
-                <label class="col-md-3 form-control-label" for="codigo">Código</label>
-                <div class="col-md-9">
-                    <input type="text" id="codigo" name="codigo" class="form-control" placeholder="Ingrese el Código" required pattern="[0-9]{0,15}">
-
-                </div>
-    </div>
-
-    <div class="form-group row">
                 <label class="col-md-3 form-control-label" for="stock">Stock</label>
                 <div class="col-md-9">
                     <input type="text" id="stock" name="stock" class="form-control" placeholder="Ingrese el stock" disabled>
@@ -39,7 +31,7 @@
      <div class="form-group row">
                 <label class="col-md-3 form-control-label" for="nombre">Nombre</label>
                 <div class="col-md-9">
-                    <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Ingrese la nombre" required pattern="^[a-zA-Z_áéíóúñ\s]{0,100}$">
+                    <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Ingrese la nombre" required>
                 </div>
     </div>
 
